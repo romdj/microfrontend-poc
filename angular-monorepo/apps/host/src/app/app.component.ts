1f3a4a137015bfccbe11ef0af8531bd7e24964d3
leshax1 <privsupport@gmail.com>
2024-04-13 21:58:09 +0700

    Update

--- angular-monorepo/apps/host/src/app/app.component.ts
+++ angular-monorepo/apps/host/src/app/app.component.ts
@@ -3,10 +3,18 @@ import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { AuthService } from '@angular-monorepo/auth';
 import { CommonModule } from '@angular/common';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { DialogOverviewExampleDialogComponent } from './components/login/login.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, CommonModule],
+  imports: [
+    NxWelcomeComponent,
+    RouterModule,
+    CommonModule,
+    MatSlideToggleModule,
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
@@ -15,5 +23,11 @@ export class AppComponent {
   title = 'host';
   name = this.authService.name;
 
-  constructor(private authService: AuthService) {}
+  openDialog(): void {
+    this.dialog.open(DialogOverviewExampleDialogComponent, {
+      data: { name: this.name },
+    });
+  }
+
+  constructor(private authService: AuthService, public dialog: MatDialog) {}
 }
---
+++ angular-monorepo/apps/host/src/app/components/login/login.component.ts
@@ -0,0 +1,40 @@
+import { Component, Inject } from '@angular/core';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+  MatDialogTitle,
+  MatDialogContent,
+  MatDialogActions,
+  MatDialogClose,
+} from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+@Component({
+  selector: 'app-overview-example-dialog',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+  ],
+})
+export class DialogOverviewExampleDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
